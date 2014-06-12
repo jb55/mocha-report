ff229493180622673322a769d6564b2d4ccc05e0
William Casarin <bill@casarin.me>
2014-06-11 18:39:09 -0700

    add some test data and fix stats in end event

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ module.exports = function(stream, Reporter) {
         var err = { message: "mocha-report: errors not yet implemented" }
         runner.emit('fail', new JsonTest(data), err)
         break;
+      case 'end':
+        reporter.stats = data
+        runner.emit('end', data)
+        break;
       default:
-        runner.emit(event, new JsonTest(data))
+        runner.emit(event, data)
     }
   });
 }
